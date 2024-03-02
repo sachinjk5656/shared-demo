4712a233ffde54baf6fb06c980c3ca029eaac145
sachin <sachinjk5151@gmail.com>
2024-03-02 15:32:25 +0530

    new changes

--- vars/firstGroovy.groovy
+++ vars/firstGroovy.groovy
@@ -1,16 +1,18 @@
 def call(String name = 'name', String place = 'place', String qualification = 'qualification')
 pipeline {
-    agent any
     environment {
         name = "$name"
         place = "$place"
         qualification = "$qualification"
     }
+    agent any
+   
 
     stages {
         stage('Printing vars') {
             steps {  
-                echo "Welcome $name from $place for completing $qualification"
+                sh 'ls'
+                sh echo "Welcome $name from $place for completing $qualification"
             }
         }
     }
